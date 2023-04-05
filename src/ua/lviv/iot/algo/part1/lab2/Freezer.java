400eae1dc98e8390a6d77ad8b273016bfb0f30d1
MartaHentosh <119297937+MartaHentosh@users.noreply.github.com>
2023-04-05 20:40:06 +0300

    Update Freezer.java

--- src/ua/lviv/iot/algo/part1/lab2/Freezer.java
+++ src/ua/lviv/iot/algo/part1/lab2/Freezer.java
@@ -1,5 +1,10 @@
 package ua.lviv.iot.algo.part1.lab2;
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 @Setter
 @Getter
 @AllArgsConstructor
