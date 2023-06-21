8e36a7897e88c29104334697fcfeb58cc362d020
Marta <119297937+MartaHentosh@users.noreply.github.com>
2023-06-21 23:55:31 +0300

    Update Freezer.java

--- src/ua/lviv/iot/algo/part1/lab2/Freezer.java
+++ src/ua/lviv/iot/algo/part1/lab2/Freezer.java
@@ -10,15 +10,29 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(callSuper = true)
+public
 class Freezer extends Fridge{
+    public static final String HEADERS = "brand, model, capacity, maxIceWeight, maxIceVolume" ;
+    public static final String value = "Prime Technics, MRF-8050W, 145, 120, 120" ;
     private double maxIceWeight;
     private double maxIceVolume;
     public  Freezer(String brand,String model, int capacity,
-                       int maxIceWeight, int maxIceVolume){
+                    int maxIceWeight, int maxIceVolume){
         super(brand,model,capacity);
         this.maxIceWeight = maxIceWeight;
         this.maxIceVolume = maxIceVolume;
     }
+
+    @Override
+    public String getHeaders() {
+        return HEADERS;
+    }
+
+    @Override
+    public String toCSV() {
+        return value;
+    }
+
     @Override
     public double getMaxUsableCapacity(){
         return maxIceWeight * maxIceVolume;
